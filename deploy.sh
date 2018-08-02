6b11b7588496690d301ada1fb88a1585f2ea2cc6
wlandau-lilly <will.landau@lilly.com>
2018-08-02 16:12:23 -0400

    Copy over README too

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ git config --global user.name "wlandau"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 make
+mv README.md gh-pages
 mv *.zip gh-pages
 cd gh-pages
-echo "https://github.com/wlandau/drake-examples" > README.md
 git add --all README.md *.zip
 git commit -m "Update example archives" || true
 git push -q origin gh-pages
