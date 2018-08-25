640e7536454f4d44b0f3ea55a18ffe275bf49643
wlandau <will.landau@gmail.com>
2018-08-25 18:39:14 -0400

    Turn the overhead example into a performance study

--- overhead/README.md
+++ overhead/README.md
@@ -1,3 +1,1 @@
-This workflow is designed to maximize overhead and stress-test drake. The goal is to improve drake's internals so this example runs fast.
-
-Let n be the number of targets. There are n * (n - 1) / 2 non-file dependency connections among all the targets, which is the maximum possible. For i = 2, ..., n, target i depends on targets 1 through i - 1.
+This workflow is designed to maximize overhead and stress-test drake. The goal is to improve drake's internals so this example runs fast. Run the `make.R` script to for some benchmarks.
--- overhead/make.R
+++ overhead/make.R
@@ -6,15 +6,46 @@
 # among all the targets, which is the maximum possible.
 # For i = 2, ..., n, target i depends on targets 1 through i - 1.
 
-n <- 4
-
 library(drake)
-library(magrittr)
-plan <- drake_plan(target_1 = 1)
-for (i in seq_len(n - 1) + 1){
-  target <- paste0("target_", i)
-  dependencies <- paste0("target_", seq_len(i - 1))
-  command <- paste0("max(", paste0(dependencies, collapse = ", "), ")")
-  plan <- rbind(plan, data.frame(target = target, command = command))
+library(microbenchmark)
+library(storr)
+
+# See vis_drake_graph(drake_config(plan(8)), targets_only = TRUE)
+
+plan <- function(n){
+  plan <- drake_plan(target_1 = 1)
+  for (i in seq_len(n - 1) + 1){
+    target <- paste0("target_", i)
+    dependencies <- paste0("target_", seq_len(i - 1))
+    command <- paste0("max(", paste0(dependencies, collapse = ", "), ")")
+    plan <- rbind(plan, data.frame(target = target, command = command))
+  }
+  plan
 }
-make(plan, verbose = 4)
+
+cache <- function(n){
+  path <- file.path(tempdir(), paste0("cache", n))
+  cache <- storr_rds(path, mangle_key = TRUE)
+  clean(cache = cache)
+  cache
+}
+
+overhead <- function(n){
+  make(plan(n), cache = cache(n), verbose = 4)
+}
+
+# Uncomment the lines below to increase the scale of the testing scenarios.
+
+benchmarks <- microbenchmark(
+  overhead(8),
+  overhead(16),
+  overhead(32),
+#  overhead(64),
+#  overhead(128),
+#  overhead(256),
+#  overhead(512),
+#  overhead(1024),
+  times = 10
+)
+
+print(benchmarks)
