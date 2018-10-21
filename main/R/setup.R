0abc840e4c58f5c64abfc3c67eceed128efc40ef
wlandau <will.landau@gmail.com>
2018-10-21 05:39:37 -0400

    Tweak a comment

--- main/R/setup.R
+++ main/R/setup.R
@@ -1,5 +1,5 @@
 # Load our packages in the usual way.
-# This example requires packages forcats and readxl,
+# This example requires packages forcats, readxl, and rmarkdown,
 # but you do not need to load them here.
 library(drake)
 library(dplyr)
