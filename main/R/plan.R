f326ecaaa53d5b7c1a47a92840e5e08823287022
wlandau <will.landau@gmail.com>
2018-10-21 05:14:38 -0400

    Split the main example into separate files

---
+++ main/R/plan.R
@@ -0,0 +1,14 @@
+# The workflow plan data frame outlines what you are going to do.
+plan <- drake_plan(
+  raw_data = readxl::read_excel(file_in("raw_data.xlsx")),
+  data = raw_data %>%
+    mutate(Species = forcats::fct_inorder(Species)) %>%
+    select(-X__1),
+  hist = create_plot(data),
+  fit = lm(Sepal.Width ~ Petal.Width + Species, data),
+  report = rmarkdown::render(
+    knitr_in("report.Rmd"),
+    output_file = file_out("report.html"),
+    quiet = TRUE
+  )
+)
