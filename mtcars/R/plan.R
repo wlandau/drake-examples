75a07703579ad944c937b3754228e549c260f59d
wlandau <will.landau@gmail.com>
2018-10-21 06:05:48 -0400

    No longer rely on plan_analyses() or plan_summaries()

--- mtcars/R/plan.R
+++ mtcars/R/plan.R
@@ -23,39 +23,34 @@ methods <- drake_plan(
 
 # Here, we use the template to expand the `methods` template
 # over the datasets we will analyze.
-# Same as evaluate(methods, wildcard = "..dataset..",
-#   values = my_datasets$target)
-my_analyses <- plan_analyses(methods, datasets = my_datasets)
+my_analyses <- evaluate_plan(
+  methods, wildcard = "dataset__",
+  values = my_datasets$target
+)
 
 # Now, we summarize each regression fit of each bootstrapped dataset.
 # We will look at these summaries to figure out if fuel efficiency
 # and weight are related somehow.
-# Again, this is a template. Later we will expand it over the
-# available regression models.
 summary_types <- drake_plan(
   summ = suppressWarnings(summary(analysis__$residuals)), # Summarize the RESIDUALS of the model fit. # nolint
   coef = suppressWarnings(summary(analysis__))$coefficients # Coefficinents with p-values # nolint
 )
 
-# Here, we expand the commands to summarize each analysis in turn.
-# summaries() also uses evaluate(): once with expand = TRUE,
-#   once with expand = FALSE
-results <- plan_summaries(
+my_summaries <- evaluate_plan(
   summary_types,
-  my_analyses,
-  my_datasets,
-  gather = NULL
-) # skip 'gather' (workflow my_plan is more readable)
+  wildcard = "analysis__",
+  values = my_analyses$target
+)
 
 # Use `knitr_in()` to tell drake to look for dependencies
 # inside report.Rmd (targets referenced explicitly with loadd() and readd()
 # in active code chunks).
 # Use file_out() to tell drake that the target is a file.
-# Drake knows to put report.md in the "target" column when it comes
+# drake knows to put report.md in the "target" column when it comes
 # time to make().
 report <- drake_plan(
   report = knit(knitr_in("report.Rmd"), file_out("report.md"), quiet = TRUE)
 )
 
 # Row order doesn't matter in the workflow my_plan.
-my_plan <- rbind(report, my_datasets, my_analyses, results)
+my_plan <- bind_plans(report, my_datasets, my_analyses, my_summaries)
