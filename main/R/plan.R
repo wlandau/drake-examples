e28ab042872225b7bfd66da6ef2fb36a04beff0d
wlandau <will.landau@gmail.com>
2019-01-01 19:16:02 -0500

    Remove column of row names in raw_data.xlsx in main example

--- main/R/plan.R
+++ main/R/plan.R
@@ -2,8 +2,7 @@
 plan <- drake_plan(
   raw_data = readxl::read_excel(file_in("raw_data.xlsx")),
   data = raw_data %>%
-    mutate(Species = forcats::fct_inorder(Species)) %>%
-    select(-X__1),
+    mutate(Species = forcats::fct_inorder(Species)),
   hist = create_plot(data),
   fit = lm(Sepal.Width ~ Petal.Width + Species, data),
   report = rmarkdown::render(
