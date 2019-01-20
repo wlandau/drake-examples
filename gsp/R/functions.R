670dac9418dc06737f0ef8614b5a8e6e7ae166ea
wlandau <will.landau@gmail.com>
2019-01-20 08:54:15 -0500

    Update gsp example

--- gsp/R/functions.R
+++ gsp/R/functions.R
@@ -1,12 +1,3 @@
-# This file contains all the functions of the workflow.
-# If needed, you could split it up into multiple files.
-
-# Fit a model given a bunch of covarites and data
-fit_gsp_model <- function(V1, V2, V3, data){
-  lm(as.formula(paste("gsp ~", V1, "+", V2, "+", V3)), data = data)
-}
-# fit_gsp_model("state", "year", "pcap", Produc) # nolint
-
 # We need to define a function to get the
 # root mean squared prediction error.
 get_rmspe <- function(lm_fit, data){
--- gsp/R/plan.R
+++ gsp/R/plan.R
@@ -8,43 +8,31 @@ head(Produc) # ?Produc
 predictors <- setdiff(colnames(Produc), "gsp")
 
 # We will try all combinations of three covariates.
-combos <- t(combn(predictors, 3))
-colnames(combos) <- c("V1", "V2", "V3")
-combos <- tibble::as_tibble(combos)
+combos <- combn(predictors, 3)
 
-# We want a `combos` data frame with all the
-# arguments to `fit_gsp_model()`
-# The `data` argument to fit_gsp_model() is a symbol,
-# which could stand for a generic dataset or an upstream target.
-combos$data <- rlang::syms(rep("Produc", nrow(combos)))
+# We have a grid of covariates.
+x1 <- rlang::syms(combos[1, ])
+x2 <- rlang::syms(combos[2, ])
+x3 <- rlang::syms(combos[3, ])
 
-# Let's get some nice target names too.
-combos$id <- apply(combos, 1, paste, collapse = "_")
-
-# Now we make a drake plan: we plan to call
-# fit_gsp_model() iteratively for each row in `combos`.
-model_plan <- map_plan(combos, fit_gsp_model)
-
-# Judge the models based on the root mean squared prediction error (RMSPE)
-rmspe_args <- tibble::tibble(
-  lm_fit = rlang::syms(model_plan$target),
-  data = rlang::syms(combos$data),
-  id = paste0("rmspe_", model_plan$target)
-)
-rmspe_plan <- map_plan(rmspe_args, get_rmspe)
-
-# Aggregate all the results together.
-rmspe_results_plan <- gather_plan(
-  plan = rmspe_plan,
-  target = "rmspe",
-  gather = "rbind"
-)
-
-# Plan some final output.
-output_plan <- drake_plan(
+# Requires drake >= 7.0.0 or the development version
+# at github.com/ropensci/drake.
+# Install with remotes::install_github("ropensci/drake").
+plan <- drake_plan(
+  model = target(
+    lm(gsp ~ x1 + x2 + x3, data = Ecdat::Produc),
+    transform = map(x1 = !!x1, x2 = !!x2, x3 = !!x3)
+  ),
+  rmspe_i = target(
+    get_rmspe(model, Ecdat::Produc),
+    transform = map(model)
+  ),
+  rmspe = target(
+    do.call(rbind, rmspe_i),
+    transform = reduce()
+  ),
   plot = ggsave(filename = file_out("rmspe.pdf"), plot = plot_rmspe(rmspe)),
   report = knit(knitr_in("report.Rmd"), file_out("report.md"), quiet = TRUE)
 )
 
-# Put together the whole plan.
-whole_plan <- rbind(model_plan, rmspe_plan, rmspe_results_plan, output_plan)
+
