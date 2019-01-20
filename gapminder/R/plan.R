4d593c398285c6fe0632c7f235712be77aba2bc4
wlandau <will.landau@gmail.com>
2019-01-20 08:36:51 -0500

    Refactor gapminder example

--- gapminder/R/plan.R
+++ gapminder/R/plan.R
@@ -1,24 +1,11 @@
-# Each row represents a model we fit to gross state product
-analysis_steps <- tibble(
-  covariate = c("life_exp", "log_pop"),
-  dataset = "dataset"
-) %>%
-  mutate(
-    id = covariate,
-    dataset = syms(dataset)
-  )
-
-# Turn the analysis steps into targets and commands.
-analysis_plan <- map_plan(analysis_steps, fit_model) %>%
-  gather_plan(target = "plots", gather = "gather_plots", append = TRUE)
-
-# Create the whole drake plan.
 plan <- drake_plan(
   dataset = prepare_gapminder(gapminder::gapminder),
   report = render(
     knitr_in("report.Rmd"),
     output_file = file_out("report.html"),
     quiet = TRUE
-  )
-) %>%
-  rbind(analysis_plan)
+  ),
+  life_exp = fit_model(covariate = "life_exp", dataset = dataset),
+  log_pop = fit_model(covariate = "log_pop", dataset = dataset),
+  plots = gather_plots(life_exp = life_exp, log_pop = log_pop)
+)
