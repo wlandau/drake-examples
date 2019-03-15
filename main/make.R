8662973d6844ffae8d66c456cb9e514964aa84a2
Will Landau <will.landau@gmail.com>
2019-03-15 16:05:40 -0400

    Update make.R

--- main/make.R
+++ main/make.R
@@ -1,15 +1,15 @@
 # Walkthrough: https://ropenscilabs.github.io/drake-manual/intro.html
-# Slides: https://krlmlr.github.io/drake-pitch
-# Code: drake_example("main") # nolint
+# Download the code: drake_example("main") # nolint
 
-# Load our packages and supporting functions into our session.
-source("R/packages.R")
-source("R/functions.R")
+# Load your packages and supporting functions into your session.
+# If you use supporting scripts like the ones below,
+# you will need to supply them yourself. Examples:
+# https://github.com/wlandau/drake-examples/tree/master/main/R
+source("R/packages.R")  # Load your packages, e.g. library(drake).
+source("R/functions.R") # Define your custom code as a bunch of functions.
+source("R/plan.R")      # Create your drake plan.
 
-# Create the `drake` plan that outlines the work we are going to do.
-source("R/plan.R")
-
-# Run your work with make().
+# Call make() to run your work.
+# Your targets will be stored in a hidden .drake/ cache,
+# and you can read them back into memory with loadd() and read().
 make(plan)
-
-# See also loadd(), readd(), vis_drake_graph(), and drake_config(). # nolint
