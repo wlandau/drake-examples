63bb5da4ea829cda06843ebe91e2b709e5cb9146
wlandau <will.landau@gmail.com>
2019-03-27 07:26:00 -0400

    Use tidy eval in plan
    easier to scale that way

--- deep-learning/R/plan.R
+++ deep-learning/R/plan.R
@@ -1,10 +1,12 @@
+dropout <- seq(0.1, 0.4, by = 0.1)
+
 plan <- drake_plan(
   data = read_csv(file_in("data/customer_churn.csv"), col_types = cols()) %>%
     initial_split(prop = 0.3),
   rec = prepare_recipe(data),
   model = target(
-    train_model(data, rec, dropout),
-    transform = map(dropout = c(0.1, 0.2, 0.3, 0.4))
+    train_model(data, rec, drop),
+    transform = map(drop = !!dropout)
   ),
   conf = target(
     confusion_matrix(data, rec, model),
