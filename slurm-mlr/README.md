4d15bed1be94a9c771bd01a3db599b2c26d658bf
pat-s <patrick.schratz@gmail.com>
2019-04-16 11:00:36 +0200

    add slurm example

---
+++ slurm-mlr/README.md
@@ -0,0 +1,9 @@
+# Disclaimer
+
+This example shows how to use `drake` to perform a benchmark analysis using two algorithms (KNN, SVM) across two datasets.
+
+The shown tuning ranges and settings of the cross-validation are not meant to make sense. 
+They are chosen so that the example finishes within 5 minutes with the specified resources.
+Runtime will differ depending on how your CPUs are.
+
+For more detailed instructions on how to use `mlr` see https://mlr.mlr-org.com/.
---
+++ slurm-mlr/benchmark.R
@@ -0,0 +1,49 @@
+# List of learning tasks
+tasks <- list(iris.task, sonar.task)
+
+# Tune wrapper for SVM
+ps_svm <- makeParamSet(
+  makeNumericParam("C",
+    lower = -5, upper = 12,
+    trafo = function(x) 2^x
+  ),
+  makeNumericParam("sigma",
+    lower = -12, upper = 3,
+    trafo = function(x) 2^x
+  )
+)
+
+ctrl_svm <- makeTuneControlRandom(maxit = 100)
+inner_svm <- makeResampleDesc("CV", iters = 5)
+svm_wrapper <- makeTuneWrapper("classif.ksvm",
+  resampling = inner_svm,
+  par.set = ps_svm, control = ctrl_svm,
+  show.info = TRUE
+)
+
+# Tune wrapper for KNN
+ps_knn <- makeParamSet(
+  makeIntegerParam("k", lower = 10, upper = 50),
+  makeIntegerParam("distance", lower = 1, upper = 50)
+)
+ctrl_knn <- makeTuneControlRandom(maxit = 100)
+inner_knn <- makeResampleDesc("CV", iters = 5)
+knn_wrapper <- makeTuneWrapper("classif.kknn",
+  resampling = inner_knn,
+  par.set = ps_knn, control = ctrl_knn,
+  show.info = TRUE
+)
+
+# Outer resampling config 
+outer <- makeResampleDesc("RepCV", reps = 20, folds = 5)
+
+# Benchmark learners and store result in separate objects
+res_svm <- benchmark_custom(
+  learners = svm_wrapper, tasks = tasks, resampling = outer,
+  measures = list(acc, ber), cpus = 17
+)
+
+res_knn <- benchmark_custom(
+  learners = knn_wrapper, tasks = tasks, resampling = outer,
+  measures = list(acc, ber), cpus = 17
+)
