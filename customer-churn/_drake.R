d3debe93a333a00812a4acb2e456b29ddd84cf0c
wlandau-lilly <will.landau@lilly.com>
2019-08-30 16:33:47 -0600

    Update ref site

--- customer-churn/_drake.R
+++ customer-churn/_drake.R
@@ -3,7 +3,7 @@
 # r_outdated() and r_predict_runtime(). For details, visit
 # https://ropenscilabs.github.io/drake-manual/projects.html#safer-interactivity
 # and
-# https://ropensci.github.io/drake/reference/r_make.html
+# https://docs.ropensci.org/drake/reference/r_make.html
 source("R/packages.R")
 source("R/functions.R")
 source("R/plan.R")
--- main/interactive.R
+++ main/interactive.R
@@ -1,6 +1,6 @@
 # This script demonstrates how to use the
 # r_*() functions described at
-# https://ropensci.github.io/drake/reference/r_make.html
+# https://docs.ropensci.org/drake/reference/r_make.html
 
 # First, define a script called _drake.R (example provided here).
 # Then, try it out.
