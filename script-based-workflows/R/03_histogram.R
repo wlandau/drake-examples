2bb7e0c2c1801faf8b09c5e942e970c4c4fe33ff
Will Landau <will.landau@gmail.com>
2019-10-22 20:19:08 -0400

    Update 03_histogram.R

--- script-based-workflows/R/03_histogram.R
+++ script-based-workflows/R/03_histogram.R
@@ -3,13 +3,12 @@
 
 munged_data <- readRDS("data/munged_data.RDS")
 
-gg <- munged_data %>%
-  ggplot() +
-    geom_histogram(
-      aes(x = Petal.Width, fill = Species),
-      binwidth = 0.25
-    ) +
-    theme_gray(20)
+gg <- ggplot(munged_data) +
+  geom_histogram(
+    aes(x = Petal.Width, fill = Species),
+    binwidth = 0.25
+  ) +
+  theme_gray(20)
 
 ggsave(
   filename = "data/Petal_Width_vs_Species.PNG",
