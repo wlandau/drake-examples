a85d615991231d5dd3e2465077b0251427599f3e
wlandau <will.landau@gmail.com>
2019-11-03 10:19:48 -0500

    Scale up

--- overhead/dynamic.R
+++ overhead/dynamic.R
@@ -1,6 +1,6 @@
 source("setup.R")
 plan <- drake_plan(
-  x = seq_len(1e3),
+  x = seq_len(1e4),
   y = target(x, dynamic = map(x))
 )
 profile(plan)
--- overhead/static.R
+++ overhead/static.R
@@ -1,5 +1,5 @@
 source("setup.R")
 plan <- drake_plan(
-  z = target(w, transform = map(w = !!seq_len(1e3)))
+  z = target(w, transform = map(w = !!seq_len(1e4)))
 )
 profile(plan)
