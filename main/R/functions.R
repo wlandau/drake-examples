3068466916fcd50b623c8ea5c9fce0a9f013985c
wlandau-lilly <will.landau@lilly.com>
2020-06-16 09:40:07 -0400

    Set a bin width

--- main/R/functions.R
+++ main/R/functions.R
@@ -1,6 +1,6 @@
 # Your custom code is a bunch of functions.
 create_plot <- function(data) {
   ggplot(data) +
-    geom_histogram(aes(x = Ozone)) +
+    geom_histogram(aes(x = Ozone), binwidth = 10) +
     theme_gray(24)
 }
