68ed3f95c993b0f32cfa82c49f33ce35531d93e0
wlandau-lilly <will.landau@lilly.com>
2020-06-16 09:39:36 -0400

    Use histogram

--- main/R/functions.R
+++ main/R/functions.R
@@ -1,6 +1,6 @@
 # Your custom code is a bunch of functions.
 create_plot <- function(data) {
   ggplot(data) +
-    geom_point(aes(x = Temp, y = Ozone)) +
+    geom_histogram(aes(x = Ozone)) +
     theme_gray(24)
 }
