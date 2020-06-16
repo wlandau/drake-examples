4b2677725baa256d554bb6bb2b8e029b1e727429
wlandau-lilly <will.landau@lilly.com>
2020-06-16 11:02:50 -0400

    Fix a param name

--- script-based-workflows/R/03_histogram.R
+++ script-based-workflows/R/03_histogram.R
@@ -4,7 +4,7 @@
 munged_data <- readRDS("data/munged_data.RDS")
 
 gg <-  ggplot(munged_data) +
-  geom_histogram(aes(x = Ozone), bindwidth = 10) +
+  geom_histogram(aes(x = Ozone), binwidth = 10) +
   theme_gray(24)
 
 ggsave(
