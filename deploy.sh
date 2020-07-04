4ebe6c1893b2a2e0cddc02dad96003d89a42ecb6
wlandau <will.landau@gmail.com>
2020-07-03 23:08:21 -0400

    Try actions

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "will.landau@gmail.com"
 git config --global user.name "wlandau"
 
 make
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+git clone -b gh-pages git@github.com:wlandau/drake-examples gh-pages
 mv README.md gh-pages
 ls *.zip > gh-pages/examples.md
 mv *.zip gh-pages
