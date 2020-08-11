2d37ad3bcb88d4637664c165154f2eba2989642f
Will Landau <will.landau@gmail.com>
2020-08-11 15:55:54 -0400

    Update make.R

--- mtcars/make.R
+++ mtcars/make.R
@@ -1,5 +1,5 @@
 # See the full tutorial at
-# https://books.ropensci.org/drake/mtcars.html.
+# https://books.ropensci.org/drake/walkthrough.html.
 
 source("R/packages.R")  # Load all the packages you need.
 source("R/functions.R") # Load all the functions into your environment.
